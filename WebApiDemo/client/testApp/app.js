02d589870fc406a552c8f8c20df7051054673836
Matt Wilhelm <mattw@stcu.org>
2014-07-30 15:30:11 -0700

    Initial commit of a project leveraging webapi and angular

---
+++ WebApiDemo/client/testApp/app.js
@@ -0,0 +1,33 @@
+﻿(function() {
+    var testApp = angular.module('testApp', []);
+
+    testApp.controller('AlertsController',
+        ['$scope', function($scope) {
+            $scope.alerts = [
+                { text: "This is bad!", type: "danger", reason: "none" },
+                { text: "This is questionable!", type: "warning", reason: "empty" },
+                { text: "This is probably informative!", type: "info", reason: "goodstuff" }
+            ];
+
+            $scope.closeAlert = function(index) {
+                $scope.alerts.splice(index, 1);
+            };
+        }]);
+
+    testApp.directive('alert', function() {
+        return {
+            restrict: 'EA',
+            templateUrl: '/client/testApp/templates/test.html',
+            transclude: true,
+            replace: true,
+            scope: {
+                reason: '=',
+                type: '@',
+                close: '&'
+            },
+            link: function(scope, element, attributes, ctrl) {
+
+            }
+        };
+    });
+})();
